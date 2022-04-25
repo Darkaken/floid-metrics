34c38668a84c1665ab7a42021406a876d1a04eec
Cristian Fazio <darkaken@Cristians-iMac.local>
2022-04-25 13:06:02 -0400

    return structure update

--- src/metrics.py
+++ src/metrics.py
@@ -1,17 +1,20 @@
 
 def metrics(list_after_third_stage, MI_TRANSACTIONS, HI_TRANSACTIONS, total_time_months):
 
-    income_months = different_month_ammount(HI_TRANSACTIONS + MI_TRANSACTIONS)
+    main_income = MI_TRANSACTIONS + HI_TRANSACTIONS
+    income_months = different_month_ammount(main_income)
 
     result = {
 
         'regularity': f"{income_months}/{total_time_months}",
-        'mainAverage' : round(sum([x['in'] for x in HI_TRANSACTIONS]) / total_time_months) + round(sum([x['in'] for x in MI_TRANSACTIONS]) / total_time_months),
+        'mainAverage' : round(sum([x['in'] for x in main_income]) / total_time_months),
         'extraAverage' : round(sum([x['in'] for x in list_after_third_stage]) / total_time_months),
+        'mainIncomeDeposit': main_income_deposit(main_income),
     }
 
-    return result
+    result['incomeByMonth']: income_by_month(self, main_income, list_after_third_stage)
 
+    return result
 
 def different_month_ammount(transaction_list):
 
@@ -23,3 +26,73 @@ def different_month_ammount(transaction_list):
             observed_months.append(int("".join(transaction["date"].split("-")[:2])))
 
     return len(observed_months)
+
+def main_income_deposit(transaction_list):
+
+    if not transaction_list:
+        return None
+
+    description_counter = dict()
+
+    for transaction in transaction_list:
+
+        try:
+            description_counter[transaction['description']] += 1
+        except:
+            description_counter[transaction['description']] = 1
+
+    return [key for key, value in description_counter.items() if value == max([value for value in description_counter.values()])][0]
+
+def income_by_month(result, main_income, extra_income):
+
+    transactions_by_month = []
+
+    for transaction in main_income:
+        date = int("".join(transaction["date"].split("-")[:2]))
+
+        try:
+            transactions_by_month[str(date)].append(transaction)
+        except:
+            transactions_by_month[str(date)] = [transaction]
+
+    for date, value in transactions_by_month.items():
+
+        result['incomeByMonth'].append({
+            'month': f'{str(date)[:3]}-{str(date[4:])}',
+            'main': round(sum(transaction['in'] for transaction in value)),
+            'extra': 0,
+            'sources': [expand_transaction(transaction, 'main') for transaction in value]
+        })
+
+    transactions_by_month = []
+
+    for transaction in extra_income:
+        date = int("".join(transaction["date"].split("-")[:2]))
+
+        try:
+            transactions_by_month[str(date)].append(transaction)
+        except:
+            transactions_by_month[str(date)] = [transaction]
+
+    for date, value in transactions_by_month.items():
+        result['incomeByMonth'].append({
+            'month': f'{str(date)[:3]}-{str(date[4:])}',
+            'extra': round(sum(transaction['in'] for transaction in value)),
+            'sources': [expand_transaction(transaction, 'main') for transaction in value]
+        })
+
+        try:
+            result['incomeByMonth'][-1]['main'] += 0
+        except:
+            result['incomeByMonth'][-1]['main'] = 0
+
+    return result
+
+
+def expand_transaction(transaction, types):
+
+    return {
+        'sender': transaction['description'],
+        'amount': transaction['in'],
+        'type': types
+    }
